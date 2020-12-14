6b1b665efbd50f453d8b4b0fd0b845c8f3ceeefb
Accio <ericantosch@web.de>
2020-12-14 19:06:01 +0100

    Benedikt.c

---
+++ assets/scripts/Benedikt.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<math.h>
+
+
+int quersumme(int n);
+
+
+
+int main(void){
+
+  int n = 0;
+  printf("Geben Sie eine f%cnfstellige Zahl an, von der Sie die Quersumme haben wollen\n", 129);
+  scanf("%d", &n);
+  printf("Quersumme: %d", quersumme(n));
+  return 0;
+}
+
+
+
+int quersumme(int n){
+  int potenz = 0;
+  int result = 0;
+  for(int i = 1; i <= 5; i++){
+    potenz = n%10^i;
+    result = result + potenz/10^(i-1);
+  }
+
+  return result;
+}
